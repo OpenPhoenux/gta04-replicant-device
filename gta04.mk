f7b078c385c130c4463f088b429a75f6959d70fb
PaulK <contact@paulk.fr>
2012-04-23 21:52:43 +0200

    Added init.gta04.rc with instructions to enable USB Mass Storage
    Signed-off-by: PaulK <contact@paulk.fr>

--- gta04.mk
+++ gta04.mk
@@ -14,6 +14,7 @@
 
 # Hardware
 PRODUCT_COPY_FILES := \
+	device/goldelico/gta04/init.gta04.rc:root/init.gta04.rc \
 	device/goldelico/gta04/vold.fstab:system/etc/vold.fstab \
 	device/goldelico/gta04/twl4030_pwrbutton.kl:system/usr/keylayout/twl4030_pwrbutton.kl \
 	device/goldelico/gta04/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
