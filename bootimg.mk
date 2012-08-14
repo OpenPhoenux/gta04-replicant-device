ac166b0c4ae8c7a1a5a799d3bf51725f959f5c04
Paul Kocialkowski <contact@paulk.fr>
2012-08-14 22:45:24 +0200

    bootimg.mk: Added instructions to build boot.scr with bootloader
    Signed-off-by: Paul Kocialkowski <contact@paulk.fr>

--- bootimg.mk
+++ bootimg.mk
@@ -2,6 +2,9 @@ LOCAL_PATH := $(call my-dir)
 
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
+INSTALLED_BOOTLOADERIMAGE_TARGET := $(PRODUCT_OUT)/bootloader.img
+INSTALLED_BOOTSCR_TARGET := $(PRODUCT_OUT)/boot.scr
+GTA04_BOOTSCR_SOURCE := device/goldelico/gta04/u-boot/boot-scr/replicant-boot.txt
 
 .NOTPARALLEL: $(INSTALLED_BOOTIMAGE_TARGET)
 .NOTPARALLEL: $(INSTALLED_RECOVERYIMAGE_TARGET)
@@ -13,3 +16,9 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_RAMDISK_TARGET) $(INSTALLED_KERNEL_TA
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(recovery_ramdisk) $(recovery_kernel) $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/mke2fs
 	$(call pretty,"Recovery image: $@")
 	$(ACP) $(PRODUCT_OUT)/kernel-recovery $@
+
+$(INSTALLED_BOOTLOADERIMAGE_TARGET): $(INSTALLED_BOOTLOADER_TARGET) $(MKIMAGE)
+	$(hide) $(ACP) -fp $(INSTALLED_BOOTLOADER_TARGET) $@
+	$(MKIMAGE) -A arm -T script -C none -n "boot.scr" -d $(GTA04_BOOTSCR_SOURCE) $(INSTALLED_BOOTSCR_TARGET)
+	$(call pretty,"Bootloader image: $@")
+	$(call pretty,"Boot-scr: $(INSTALLED_BOOTSCR_TARGET)")
