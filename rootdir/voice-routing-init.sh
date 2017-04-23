206abfa33d1b9eaadee2246faed04b82c7331240
Andreas Kemnade <andreas@kemnade.info>
2017-04-23 13:07:49 +0200

    rootdir: fix mixer settings file selection
    formerly the presence of gpio 186 was used to detect whether hw or
    sw routing setting was needed.
    Now use model name from devicetree instead.
    
    amixer | grep some_control fails because
    amixer is not available
    
    Signed-off-by: Andreas Kemnade <andreas@kemnade.info>

--- rootdir/voice-routing-init.sh
+++ rootdir/voice-routing-init.sh
@@ -1,17 +1,19 @@
 #!/system/bin/sh
-MODEM_GPIO="/sys/class/gpio/gpio186/value" #available on GTA04a4++
-if [ ! -e $MODEM_GPIO ]
-then
+case "$(cat /proc/device-tree/model)" in
+	*GTA04A[1-3]* ) 
     # Software Voice Routing
     echo "LOADING tinyalsa-audio_swrouting.xml"
     rm /system/etc/tinyalsa-audio.xml
     cp /system/etc/tinyalsa-audio_swrouting.xml /system/etc/tinyalsa-audio.xml
-else
+    ;;
+*GTA04A[4-9]* )
     # Hardware Voice Routing
     echo "LOADING tinyalsa-audio_hwrouting.xml"
     rm /system/etc/tinyalsa-audio.xml
     cp /system/etc/tinyalsa-audio_hwrouting.xml /system/etc/tinyalsa-audio.xml
-fi
+    ;;
+esac
+
 chown 502.50 /system/etc/tinyalsa-audio.xml
 chmod 644 /system/etc/tinyalsa-audio.xml
 
